c6a1e7c2a85a2abca97090275cb21891b2944013
WebDevJoe <joegentswebdev@gmail.com>
2023-11-20 16:44:03 +0000

    updates to validation and db creation

--- website/auth.py
+++ website/auth.py
@@ -19,20 +19,22 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email)>4:
-            flash('email must be greater than 4 characters', category='error')
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
             
-        elif len(firstName) <2:
-             flash('First name must be greater than 2 characters', category='error')
+        elif len(firstName) < 2:
+             flash('First name must be greater than 1 character', category='error')
             
         elif password1 != password2:
             flash('Your passwords do not match', category='error')
            
-        elif len(password1) <7:
+        elif len(password1) < 7:
             flash('password must be greater than 7 characters', category='error')
        
         else: 
-            flash('Account created!, category="success')
+            flash('Account created!', category='success')
             pass
+     
+
 
     return render_template("sign_up.html")
