31d59bff69b61af11d4cfdc511d526476ff0a42f
WebDevJoe <joegentswebdev@gmail.com>
2023-11-20 15:50:10 +0000

    getting data from form

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET', 'POST'])
 def login():
+  
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -12,4 +13,9 @@ def logout():
 
 @auth.route('/sign-up',methods=['GET', 'POST'])
 def sign_up():
+    if request.method =='POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
     return render_template("sign_up.html")
