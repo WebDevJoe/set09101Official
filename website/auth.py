3d4d08cb9c4fdedc8195f781549d755e09cc65db
WebDevJoe <joegentswebdev@gmail.com>
2023-11-20 15:58:10 +0000

    form validation

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -18,4 +18,21 @@ def sign_up():
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        if len(email)>4:
+            flash('email must be greater than 4 characters', category='error')
+            
+        elif len(firstName) <2:
+             flash('First name must be greater than 2 characters', category='error')
+            
+        elif password1 != password2:
+            flash('Your passwords do not match', category='error')
+           
+        elif len(password1) <7:
+            flash('password must be greater than 7 characters', category='error')
+       
+        else: 
+            flash('Account created!, category="success')
+            pass
+
     return render_template("sign_up.html")
