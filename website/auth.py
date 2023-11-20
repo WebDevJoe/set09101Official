07b2d6cc8af17b0c5186ef07574ec1775f5a9a5f
WebDevJoe <joegentswebdev@gmail.com>
2023-11-20 14:46:13 +0000

    making urls

--- website/auth.py
+++ website/auth.py
@@ -4,11 +4,11 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p> Login</p>"
+    return "<p>Login</p>"
 
 @auth.route('/logout')
 def logout():
-    return "<p> Log out</p>"
+    return "<p>Logout</p>"
 
 @auth.route('/sign-up')
 def sign_up():
