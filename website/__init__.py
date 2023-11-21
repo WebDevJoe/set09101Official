b732e69c791396cee21817a9843c1ea066edcb33
WebDevJoe <joegentswebdev@gmail.com>
2023-11-21 14:05:15 +0000

    updates

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY']='haha'
+    app.config['SECRET_KEY'] = 'haha'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,5 +17,24 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created database!')
+    else:
+        print('Database file already exists.')
+
+if __name__ == '__main__':
+    app = create_app()
+    create_database(app)
+    app.run(debug=True)
+
+    
+
+
 
