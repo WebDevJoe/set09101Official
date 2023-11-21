c04ad337c29a7f266f26aaf178d44311b8cd9d6d
WebDevJoe <joegentswebdev@gmail.com>
2023-11-21 14:14:00 +0000

    models push

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'haha'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.abspath("website/" + DB_NAME)}'
     db.init_app(app)
 
     from .views import views
@@ -22,7 +22,10 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    db_path = path.abspath("website/" + DB_NAME)
+    print("Database Path:", db_path)
+
+    if not path.exists(db_path):
         with app.app_context():
             db.create_all()
             print('Created database!')
@@ -33,6 +36,7 @@ if __name__ == '__main__':
     app = create_app()
     create_database(app)
     app.run(debug=True)
+
 
     
 
